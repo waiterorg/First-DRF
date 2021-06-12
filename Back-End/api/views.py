97ea9e39b767819820df24d79733f3738f95fb03
waiterorg <konjkav79@gmail.com>
2021-06-12 11:08:12 +0430

    Add permissions to users views.

--- Back-End/api/views.py
+++ Back-End/api/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAdminUser
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 
@@ -17,7 +18,9 @@ class ArticleDetail(RetrieveAPIView):
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
 
 class UserDetail(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
