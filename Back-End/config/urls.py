c6664ee3c6c66cffdfe952018b59818c1d941475
waiterorg <konjkav79@gmail.com>
2021-06-17 16:53:47 +0430

    create revoke view with apiview.

--- Back-End/config/urls.py
+++ Back-End/config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from api.views import RevokeToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +24,5 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('api/token-auth/', obtain_auth_token),
-
+    path('api/revoke/', RevokeToken.as_view()),
 ]
