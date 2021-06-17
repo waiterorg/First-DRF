de8ce4c638d1b1f2582ba6a7b5f09355df8249ad
waiterorg <konjkav79@gmail.com>
2021-06-17 16:54:13 +0430

    create revoke view.

--- Back-End/api/views.py
+++ Back-End/api/views.py
@@ -1,7 +1,9 @@
 #from django.shortcuts import render
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
@@ -25,3 +27,10 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsSuperUserOrStaffReadOnly,)
+
+class RevokeToken(APIView):
+    permission_classes = (IsAuthenticated,)
+    
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=204)
