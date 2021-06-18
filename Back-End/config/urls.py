ca4a2611387880b5d5a085af42bc6d4c8b92e142
waiterorg <konjkav79@gmail.com>
2021-06-18 22:28:35 +0430

    JWT with dj-rest-auth.

--- Back-End/config/urls.py
+++ Back-End/config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from dj_rest_auth.views import PasswordResetConfirmView
 
-# from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,8 +25,8 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('api/rest-auth/', include('dj_rest_auth.urls')),
-    # path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    # path('api/rest-auth/password/reset/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/rest-auth/password/reset/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
