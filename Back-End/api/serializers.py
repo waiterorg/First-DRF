b323ce3ca4230b4e8046afb6c896faafb007cf53
waiterorg <konjkav79@gmail.com>
2021-06-20 12:36:22 +0430

    write article title validate.

--- Back-End/api/serializers.py
+++ Back-End/api/serializers.py
@@ -7,6 +7,13 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = "__all__"
 
+    def validate_title(self, value):
+        filter_list = ["php","laravel"]
+        
+        for i in filter_list:
+            if i in value:
+                raise serializers.ValidationError("stop it, get some help! dont use {}".format(i))
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
